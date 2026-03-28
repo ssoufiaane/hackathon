300cf2e872fe57c545161713d95a47e9c3922299
soooufiane <ytsoyoto@gmail.com>
2026-03-28 06:22:27 +0100

    first commit

---
+++ city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="city" tilewidth="24" tileheight="16" tilecount="462" columns="21">
+ <image source="spr_tile_itch.png" trans="000000" width="512" height="352"/>
+</tileset>

73f0fa5c2724b69ce15e179d26604478069a7df1
soooufiane <ytsoyoto@gmail.com>
2026-03-28 09:00:03 +0100

    first commit

---
+++ city 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="city 2" tilewidth="24" tileheight="16" tilecount="2688" columns="42">
+ <image source="CP_V1.0.4.png" trans="000000" width="1024" height="1024"/>
+</tileset>
